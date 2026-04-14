6e00ba36875d1f6230890afd37670cca367e441f
gkak12 <hyunjun5681@kakao.com>
2026-04-14 19:46:47 +0900

    DB 접속 정보 및 UserDetails 메서드 재정의

--- bff-service/src/main/java/com/commerce/bff/security/CustomUserDetails.java
+++ bff-service/src/main/java/com/commerce/bff/security/CustomUserDetails.java
@@ -34,4 +34,24 @@ public class CustomUserDetails implements UserDetails {
     public String getEmail() {
         return user.getEmail();
     }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
 }
